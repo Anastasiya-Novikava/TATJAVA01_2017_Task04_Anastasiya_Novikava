66ba0873240cde2b03103ddc5e1c77c0367acd6b
Anastasiya-Novikava <novikova.anastasiya2012@yandex.ru>
2017-02-17 02:19:46 +0300

    Update CommandName.java

--- Task 4. JDBC/src/com/epam/task4/controller/command/CommandName.java
+++ Task 4. JDBC/src/com/epam/task4/controller/command/CommandName.java
@@ -2,9 +2,9 @@ package com.epam.task4.controller.command;
 
 public enum  CommandName {
 
-	REGISTRATION("Registration"),
-	SIGN_IN("Sign_in"),
-	SIGN_OUT("Sign_out"),
+    REGISTRATION("Registration"),
+    SIGN_IN("Sign_in"),
+    SIGN_OUT("Sign_out"),
     ADD_NEWS("Add_news"),
     FIND_BY_TITLE("Find_by_title"),
     FIND_BY_CATEGORY("Find_by_category"),
